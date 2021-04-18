c60f51e5d8170b6e47f13b6de50df883641884e4
Ubuntu <ubuntu@docker.tuimac.private>
2021-04-18 15:14:41 +0000

    before add the picture of architechture

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws cloudformation create-stack --stack-name s3proxy \
-    --region ap-northeast-1 \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --template-body file://template.yml
